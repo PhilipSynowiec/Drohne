07c5e8be720896f1264eead064193942cc434fec
PhilipSynowiec <philip@synowiec.de>
2026-05-13 08:36:27 +0200

    skibidi

--- drone/main.py
+++ drone/main.py
@@ -11,12 +11,12 @@ while True:
     state = receiver.receive_state()
     if state is not None:
         throttle, roll, pitch, yaw, armed, mode = state
-    if ticks_diff(now, last_print) >= 0:
-        if state is not None:
+    if state is not None:
             print(f"pitch={pitch:.2f}, roll={roll:.2f}")
             print(f"throttle={throttle:.2f}, yaw={yaw:.2f}\n")
-        else:
-            pass # print("no data received\n")
+    if ticks_diff(now, last_print) >= 500:
+        if state is None:
+            print("no data received\n")
         last_print = now
         receiver.led.toggle()
     sleep(0.001)
